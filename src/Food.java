aa694ce1a5c46abdfe2235b2066f8a32f24df040
AlexandrStopkin <stnalex1@gmail.com>
2023-03-14 14:26:03 +0100

    Add getters and setters

--- src/Food.java
+++ src/Food.java
@@ -14,6 +14,22 @@
 //для негазированного - "скр",
 //а для алкогольного сначала спрашивает возраст и издаёт звук, только если возраст больше 18.
 public class Food {
+  private String name;
+  private int calories;
 
+  public void setName(String name) {
+    this.name = name;
+  }
 
+  public String getName() {
+    return this.name;
+  }
+
+  public void setCalories(int calories) {
+    this.calories = calories;
+  }
+
+  public int getCalories(){
+    return this.calories;
+  }
 }
