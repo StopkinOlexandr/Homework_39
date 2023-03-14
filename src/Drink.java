29ceed28cc387d3a402a106412d8555c49006507
AlexandrStopkin <stnalex1@gmail.com>
2023-03-14 14:35:16 +0100

    Add class Drink

---
+++ src/Drink.java
@@ -0,0 +1,5 @@
+public class Drink {
+  boolean gas;
+  boolean alcohol;
+
+}
--- src/Food.java
+++ src/Food.java
@@ -29,16 +29,16 @@ public class Food {
     this.calories = calories;
   }
 
-  public int getCalories(){
+  public int getCalories() {
     return this.calories;
   }
 
-  public Food (String name, int calories) {
+  public Food(String name, int calories) {
     this.name = name;
     this.calories = calories;
   }
 
-  public void eatFood (String name) {
+  public void eatFood(String name) {
     System.out.println("You have eat " + this.name + " and get " + this.calories + " calories");
   }
 }
